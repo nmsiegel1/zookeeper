3403852d0960a6f1f9f77191498a573f6e801c76
Neenyo <102773691+nmsiegel1@users.noreply.github.com>
2022-06-22 21:59:17 -0400

    add the front end

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ app.use(express.urlencoded({etended: true}));
 // parse incoming JSON data
 app.use(express.json());
 
+app.use(express.static('public'));
+
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
 
@@ -114,6 +116,22 @@ app.post('/api/animals', (req, res) => {
 
         res.json(animal);
     }
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.listen(PORT, () => {
